5943096135d7eb7862174980f8729f854e01b6d8
huzidaha <huzidaha@126.com>
2017-04-01 15:54:47 +0800

    update comment

--- comment-app3/src/reducers/comments.js
+++ comment-app3/src/reducers/comments.js
@@ -10,12 +10,15 @@ export default function (state, action) {
   }
   switch (action.type) {
     case INIT_COMMNETS:
+      // 初始化评论
       return { comments: action.comments }
     case ADD_COMMENT:
+      // 新增评论
       return {
         comments: [...state.comments, action.comment]
       }
     case DELETE_COMMENT:
+      // 删除评论
       return {
         comments: [
           ...state.comments.slice(0, action.commentIndex),
